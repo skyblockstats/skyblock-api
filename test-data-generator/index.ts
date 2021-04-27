bd5ea37ae1413c56c94822077cbf2bd20f089eca
mat <github@matdoes.dev>
2021-04-27 13:15:48 -0500

    add more to test/data/mojang.json

--- test-data-generator/index.ts
+++ test-data-generator/index.ts
@@ -3,6 +3,7 @@
  */
 
 import * as hypixelApi from '../src/hypixelApi'
+import * as mojang from '../src/mojang'
 import fs from 'fs/promises'
 import path from 'path'
 
@@ -13,22 +14,31 @@ const playerUuids = [
 	'e403573808ad45ddb5c48ec7c4db0144',
 ]
 
+async function writeTestData(requestPath: string, name: string, contents: any) {
+	const dir = path.join(__dirname, '..', 'test', 'data', requestPath)
+	await fs.mkdir(dir, { recursive: true })
+	await fs.writeFile(path.join(dir, `${name}.json`), JSON.stringify(contents, null, 2))
+}
+
 async function addResponse(requestPath: string, args: { [ key: string ]: string }, name: string) {
 	const response = await hypixelApi.sendApiRequest({
 		path: requestPath,
 		args: args,
 		key: hypixelApi.chooseApiKey()
 	})
-	const dir = path.join(__dirname, '..', 'test', 'data', requestPath)
-	await fs.mkdir(dir, { recursive: true })
-	await fs.writeFile(path.join(dir, `${name}.json`), JSON.stringify(response, null, 2))
+	await writeTestData(requestPath, name, response)
 }
 
 async function main() {
+	const uuidsToUsername = {}
 	for (const playerUuid of playerUuids) {
 		await addResponse('player', { uuid: playerUuid }, playerUuid)
 		await addResponse('skyblock/profiles', { uuid: playerUuid }, playerUuid)
+		const { username: playerUsername } = await mojang.profileFromUuid(playerUuid)
+		uuidsToUsername[playerUuid] = playerUsername
 	}
+
+	await writeTestData('', 'mojang', uuidsToUsername)
 }
 
 main()
