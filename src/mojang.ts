0cb8e5093f4bfb81794b161d432c32fef35ed628
mat <git@matdoes.dev>
2024-01-13 17:27:34 -0600

    Revert "use alternative mojang api"
    This reverts commit de342b607d8fe45d0c20a73e51c8d29b479d72df.

--- src/mojang.ts
+++ src/mojang.ts
@@ -9,7 +9,7 @@ import type { Response as UndiciResponse } from 'undici/types/fetch'
 
 // We need to create an agent to prevent memory leaks
 const httpsAgent = new Agent({
-	keepAlive: true,
+	keepAlive: true
 })
 
 interface MojangApiResponse {
@@ -21,15 +21,13 @@ interface MojangApiResponse {
 /**
  * Get mojang api data from the session server
  */
-export let profileFromUuid = async function profileFromUuid(
-	uuid: string
-): Promise<MojangApiResponse> {
+export let profileFromUuid = async function profileFromUuid(uuid: string): Promise<MojangApiResponse> {
 	let fetchResponse: UndiciResponse
 
 	try {
 		fetchResponse = await fetch(
 			// using mojang directly is faster than ashcon, also there seem to be no ratelimits here?
-			`https://mowojang.matdoes.dev/${undashUuid(uuid)}`
+			`https://sessionserver.mojang.com/session/minecraft/profile/${undashUuid(uuid)}`,
 		)
 	} catch {
 		// if there's an error, wait a second and try again
@@ -52,19 +50,20 @@ export let profileFromUuid = async function profileFromUuid(
 	}
 	return {
 		uuid: data.id,
-		username: data.name,
+		username: data.name
 	}
 }
 
-export let profileFromUsername = async function profileFromUsername(
-	username: string
-): Promise<MojangApiResponse> {
+
+export let profileFromUsername = async function profileFromUsername(username: string): Promise<MojangApiResponse> {
 	// since we don't care about anything other than the uuid, we can use /uuid/ instead of /user/
 
 	let fetchResponse: UndiciResponse
 
 	try {
-		fetchResponse = await fetch(`https://mowojang.matdoes.dev/${username}`)
+		fetchResponse = await fetch(
+			`https://api.mojang.com/users/profiles/minecraft/${username}`,
+		)
 	} catch {
 		// if there's an error, wait a second and try again
 		await sleep(1000)
@@ -75,29 +74,56 @@ export let profileFromUsername = async function profileFromUsername(
 	const rawData = await fetchResponse.text()
 	try {
 		data = JSON.parse(rawData)
-	} catch {}
+	} catch { }
+
+
+	if (!data?.id) {
+		// return { uuid: null, username: null }
+		return await profileFromUsernameAlternative(username)
+	}
 
 	return {
 		uuid: data.id,
-		username: data.name,
+		username: data.name
 	}
 }
 
-export let profileFromUser = async function profileFromUser(
-	user: string
-): Promise<MojangApiResponse> {
-	if (isUuid(user)) {
-		return await profileFromUuid(user)
-	} else return await profileFromUsername(user)
+export async function profileFromUsernameAlternative(username: string): Promise<MojangApiResponse> {
+	let fetchResponse: UndiciResponse
+
+	try {
+		fetchResponse = await fetch(
+			`https://api.ashcon.app/mojang/v2/user/${username}`,
+		)
+	} catch {
+		// if there's an error, wait a second and try again
+		await sleep(1000)
+		return await profileFromUsernameAlternative(username)
+	}
+
+	let data
+	try {
+		data = await fetchResponse.json()
+	} catch {
+		return { uuid: null, username: null }
+	}
+	if (!data.uuid)
+		return { uuid: null, username: null }
+	return {
+		uuid: undashUuid(data.uuid),
+		username: data.username
+	}
 }
 
+export let profileFromUser = async function profileFromUser(user: string): Promise<MojangApiResponse> {
+	if (isUuid(user)) {
+		return await profileFromUuid(user)
+	} else
+		return await profileFromUsername(user)
+}
+
+
 // this is necessary for mocking in the tests because es6
-export function mockProfileFromUuid($value) {
-	profileFromUuid = $value
-}
-export function mockProfileFromUsername($value) {
-	profileFromUsername = $value
-}
-export function mockProfileFromUser($value) {
-	profileFromUser = $value
-}
+export function mockProfileFromUuid($value) { profileFromUuid = $value }
+export function mockProfileFromUsername($value) { profileFromUsername = $value }
+export function mockProfileFromUser($value) { profileFromUser = $value }
